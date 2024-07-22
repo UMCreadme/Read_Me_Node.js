eefb2d018a51ec1a51f942f25f2ee45da792c34f
OSSU <o.ss.u2056@gmail.com>
2024-07-23 04:36:53 +0900

    ✨ Feat(#1): 책상세 > 쇼츠 상세 정보 리스트 조회 서비스로직 구현 - totalPages 값으로 책과 관련한 쇼츠 조회가 끝났는지 확인 후 끝났다면 해당하는 카테고리의 쇼츠가 반환되도록 구현 - 책 관련 쇼츠가 있을 경우 해당하는 쇼츠 정보 잔환하도록 구현

--- src/shorts/shorts.service.js
+++ src/shorts/shorts.service.js
@@ -1,5 +1,5 @@
 import { pageInfo } from "../../config/pageInfo.js";
-import { getShortsDetailToCategory } from "./shorts.dao.js";
+import { countShortsDetailToBook, getBookCategory, getShortsDetailToBook, getShortsDetailToCategory, getShortsDetailToCategoryExcludeBook } from "./shorts.dao.js";
 import { getShortsDetailListDto } from "./shorts.dto.js";
 
 export const getShortsDetailHome = async (category, page=1, size=10) => {
@@ -15,9 +15,29 @@ export const getShortsDetailSearch = async (category, keyword, page=1, size=10) 
     return {"data": "검색", "pageInfo": pageInfo(page, size, true)};
 }
 
-export const getShortsDetailBook = async (category, bookId, page=1, size=10) => {
+export const getShortsDetailBook = async (bookId, page=1, size=10) => {
+    const totalPages = Math.ceil(await countShortsDetailToBook(bookId) / size);
+    const lastCount = await countShortsDetailToBook(bookId) % size;
+    let result; let hasNext;
 
-    return {"data": "책", "pageInfo": pageInfo(page, size, true)};
+    const category = await getBookCategory(bookId);
+
+    if(page > totalPages) {
+        result = await getShortsDetailToCategoryExcludeBook(category, bookId, size+1, (page-totalPages-1)*size + lastCount);
+        hasNext = result.length > size;
+        if(hasNext) result.pop();
+    } else {
+        result = await getShortsDetailToBook(bookId, size+1, (page-1)*size);
+        hasNext = result.length > size;
+        if(hasNext) result.pop();
+        else {
+            result.push(...await getShortsDetailToCategoryExcludeBook(category, bookId, size - result.length + 1, 0));
+            hasNext = result.length > size;
+            if(hasNext) result.pop();
+        }
+    }
+
+    return {"data": getShortsDetailListDto(result), "pageInfo": pageInfo(page, size, hasNext)};
 }
 
 export const getShortsDetailUser = async (category, userId, page=1, size=10) => {
