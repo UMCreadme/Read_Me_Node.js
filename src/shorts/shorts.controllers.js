798ea365dbf938e1e5d60fd1b80a708e9f049a55
OSSU <o.ss.u2056@gmail.com>
2024-07-23 05:16:25 +0900

    ✨ Feat(#1): 필요한 쿼리 파람 없을 경우 예외 처리 추가

--- src/shorts/shorts.controllers.js
+++ src/shorts/shorts.controllers.js
@@ -6,7 +6,7 @@ export const getShortsDetail = async (req, res, next) => {
     const { category, keyword, book, user, like, page=1, size=10 } = req.query;
     let shorts;
 
-    if (keyword !== undefined) {
+    if (keyword !== undefined && category !== undefined) {
         shorts = await getShortsDetailSearch(category, keyword, parseInt(page), parseInt(size));
     } else if (book !== undefined) {
         shorts = await getShortsDetailBook(book, parseInt(page), parseInt(size));
@@ -14,8 +14,10 @@ export const getShortsDetail = async (req, res, next) => {
         shorts = await getShortsDetailUserLike(user, parseInt(page), parseInt(size));
     } else if (user !== undefined) {
         shorts = await getShortsDetailUser(user, parseInt(page), parseInt(size));
-    } else {
+    } else if (category !== undefined) {
         shorts = await getShortsDetailHome(category, parseInt(page), parseInt(size));
+    } else {
+        throw new BaseError(status.BAD_REQUEST);
     }
 
     res.send(response(status.SUCCESS, shorts.data, shorts.pageInfo));
