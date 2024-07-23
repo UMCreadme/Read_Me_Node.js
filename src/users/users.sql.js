d74262d247298e3f32032b097dffdbf3eb4f18c9
ajwoong <sosloahn@gmail.com>
2024-07-23 23:02:16 +0900

    refactor: 유저가 읽은 책 리스트 조회 수정 - 최신순으로 정렬 부분 수정

--- src/users/users.sql.js
+++ src/users/users.sql.js
@@ -2,11 +2,11 @@ export const getUserById = "SELECT * FROM users WHERE user_id = ?";
 export const getUserFollowings = "SELECT * FROM follow WHERE follower = ?"
 export const getUserFollowers = "SELECT * FROM follow WHERE user_id = ?"
 
-export const getUserShortsById = "SELECT * FROM shorts WHERE user_id = ? LIMIT ? OFFSET ?";
+export const getUserShortsById = "SELECT * FROM shorts WHERE user_id = ? ORDER BY CREATED_AT DESC LIMIT ? OFFSET ?";
 
-export const getUserLikeShortsIdById = "SELECT shorts_id FROM like_shorts WHERE user_id = ? LIMIT ? OFFSET ?"
+export const getUserLikeShortsIdById = "SELECT shorts_id FROM like_shorts WHERE user_id = ? ORDER BY CREATED_AT DESC LIMIT ? OFFSET ?"
 
-export const getUserReadBooksIdById = "SELECT book_id FROM user_book WHERE user_id = ? LIMIT ? OFFSET ?"
+export const getUserReadBooksIdById = "SELECT book_id FROM user_book WHERE user_id = ? ORDER BY CREATED_AT DESC LIMIT ? OFFSET ?"
 
 export const getImageById = "SELECT * FROM image WHERE image_id = ?"
 
