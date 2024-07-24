250cf41a1e6a2824338f94bc5cee20f394175661
OSSU <o.ss.u2056@gmail.com>
2024-07-24 17:11:04 +0900

    ✨ Feat(#7): 쇼츠 검색 api 경로 추가

--- src/shorts/shorts.route.js
+++ src/shorts/shorts.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getShortsDetail } from './shorts.controller.js';
+import { getShortsDetail, searchShorts } from './shorts.controller.js';
 
-export const shortsRouter = express.Router();
+export const shortsRouter = express.Router({mergeParams:true});
 
 shortsRouter.get('/detail', asyncHandler(getShortsDetail));
+shortsRouter.get('', asyncHandler(searchShorts));
