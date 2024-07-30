d0fca32a86577de56c7e2485a085aefe11353684
OSSU <o.ss.u2056@gmail.com>
2024-07-30 16:21:54 +0900

    ✨ Feat(#38): 책 상세 정보 조회 API 라우터 & 컨트롤러 구현

---
+++ src/book/book.controller.js
@@ -0,0 +1,13 @@
+import { response } from "../../config/response.js";
+import { status } from "../../config/response.status.js";
+import { getBookDetailInfo } from "./book.service.js";
+
+export const getBookDetail = async (req, res, next) => {
+    const ISBN = req.params.ISBN;
+    const { page=1, size=10 } = req.query;
+    const userId = 1; // TODO: 유저 인가 구현 후 수정
+    
+    const book = await getBookDetailInfo(parseInt(ISBN), parseInt(page), parseInt(size), userId);
+
+    res.send(response(status.SUCCESS, book.data, book.pageInfo));
+}
