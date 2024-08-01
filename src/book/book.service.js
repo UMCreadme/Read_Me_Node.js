ca45f0229d4da6f6becdd5b0a8a8cf56b38104c8
ajwoong <sosloahn@gmail.com>
2024-08-01 16:07:36 +0900

    Feat: 최근 선택한 책 리스트 조 - 유저가 쇼츠 생성시 선택했던 책 최신순 조회 리스트

--- src/book/book.service.js
+++ src/book/book.service.js
@@ -1,6 +1,6 @@
 import {findUserRecentBookList} from "./book.dao.js";
 
 export const findUserRecentBook = async (body, offset, limit) => {
-    const userId = body.id
+    const userId = body.id //TODO 로그인 미들웨어 추가하면 변경
     return await findUserRecentBookList(userId, offset, limit)
 }
