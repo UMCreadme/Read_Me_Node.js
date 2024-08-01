0c799eae04dbb1287b95369f080b180dc65f6860
Sunho12 <rhkrtjsgh1210@gmail.com>
2024-08-01 14:52:13 +0900

    :sparkles: 24시간 이내 피드 조회 구현 + 쿼리문 수정

--- src/home/home.sql.js
+++ src/home/home.sql.js
@@ -1,11 +1,11 @@
 export const getShortsByCategory = 
 `SELECT
-    u.user_id AS user_id, u.image_url AS profileImg, u.nickname AS nickname,
-    s.shorts_id AS shorts_id, s.book_id AS book_id, s.image_url AS shortsImg,
-    s.phrase AS phrase, s.title AS title, s.content AS content, s.tag AS tags,
+    u.user_id, u.image_url AS profileImg, u.nickname,
+    s.shorts_id, s.book_id, s.image_url AS shortsImg,
+    s.phrase, s.title, s.content, s.tag AS tags,
     (SELECT COUNT(*) FROM LIKE_SHORTS ls WHERE ls.shorts_id = s.shorts_id) AS likeCnt,
     (SELECT COUNT(*) FROM COMMENT c WHERE c.shorts_id = s.shorts_id) AS commentCnt,
-    s.created_at AS created_at
+    s.created_at
 FROM USERS u
 JOIN SHORTS s ON u.user_id = s.user_id
 JOIN BOOK b ON s.book_id = b.book_id
@@ -64,5 +64,6 @@ EXISTS(SELECT 1 FROM LIKE_SHORTS WHERE shorts_id = s.shorts_id AND user_id = ?) 
 JOIN FOLLOW f on u.user_id = f.follower 
 JOIN SHORTS s ON u.user_id = s.user_id 
 WHERE f.user_id = ?
+AND s.created_at >= NOW() - INTERVAL 24 HOUR
 ORDER BY likeCnt DESC LIMIT ? OFFSET ?;
 `;
