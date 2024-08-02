b119bcc536f42bfdfdc978b8badc0f92ddf2ff15
Sunho12 <rhkrtjsgh1210@gmail.com>
2024-08-02 20:19:23 +0900

    ✨ Feat(#54): 헬스 체크용 api 구현

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import { shortsRouter } from './src/shorts/shorts.route.js';
 import { homeRouter } from './src/home/home.route.js';
 import { bookRouter } from './src/book/book.route.js';
 import { communitiesRouter } from './src/communities/communities.route.js';
+import { healthRouter } from './src/health/health.route.js';
 
 dotenv.config();
 
@@ -30,6 +31,7 @@ app.use('/shorts', shortsRouter);
 app.use('/home', homeRouter);
 app.use('/books', bookRouter);
 app.use('/communities', communitiesRouter);
+app.use('/health', healthRouter)
 
 
 // index.js
