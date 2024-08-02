a4bacf1cb46c7c212d19220b29a6a7ee4ca4a84d
Sunho12 <rhkrtjsgh1210@gmail.com>
2024-08-02 23:59:12 +0900

    :recycle: 변수명 수정

--- src/home/home.controller.js
+++ src/home/home.controller.js
@@ -5,11 +5,11 @@ import { ShortsByCategory, getMainInfo } from "./home.service.js";
 
 // 카테고리 탭별 숏츠 리스트 조회
 export const getCategoryShorts = async (req, res, next) => {
-    const category_id = req.query.category;
+    const category_name = req.query.category;
     const page = parseInt(req.query.page) || 1;
     const size = parseInt(req.query.size) || 20;
     const offset = (page - 1) * size;
-    const result = await ShortsByCategory(category_id, offset, size+1);
+    const result = await ShortsByCategory(category_name, offset, size+1);
 
     const hasNext = result.length > size;
     if (hasNext) result.pop();
