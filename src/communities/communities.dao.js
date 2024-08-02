828561d830e647e8bbd5d99a79731b4c7017f54a
urimida <crystal5k123@gmail.com>
2024-08-02 22:09:25 +0900

    Feat(#10): pageinfo 오류 수정, 방장 admin 설정

--- src/communities/communities.dao.js
+++ src/communities/communities.dao.js
@@ -11,8 +11,8 @@ export const createCommunity = async (userId, bookId, address, tag, capacity) =>
 
 // 방장을 커뮤니티에 추가하는 함수
 export const addAdminToCommunity = async (communityId, userId) => {
-    const query = 'INSERT INTO COMMUNITY_USERS (community_id, user_id) VALUES (?, ?)';
-    const values = [communityId, userId];
+    const query = 'INSERT INTO COMMUNITY_USERS (community_id, user_id, role) VALUES (?, ?, ?)';
+    const values = [communityId, userId, 'admin'];
     const [result] = await pool.query(query, values);
     return result;
 };
