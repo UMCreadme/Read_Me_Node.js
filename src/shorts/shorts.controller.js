97fd83213f8cc12b7990a2a0fd23450de25d0876
Claire <113418806+Sunho12@users.noreply.github.com>
2024-08-05 15:50:36 +0900

    변수명 수정

--- src/shorts/shorts.controller.js
+++ src/shorts/shorts.controller.js
@@ -78,7 +78,7 @@ export const addComment = async (req, res, next) => {
 
 export const likeShorts = async (req, res, next) => {
     const shorts_id = req.params.shortsId;
-    const { user_id } = req.userId;
+    const { user_id } = req.user_id;
 
     if (!shorts_id || !user_id) {
         return next(new BaseError(status.BAD_REQUEST));
