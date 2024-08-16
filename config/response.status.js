de334038c9975a8b6c7c065165d7764fd871c53e
ajwoong <sosloahn@gmail.com>
2024-08-16 19:00:40 +0900

    Bug: missing token 에러 대신 unauthorized로 변경

--- config/response.status.js
+++ config/response.status.js
@@ -48,7 +48,6 @@ export const status = {
     NOT_EXISTING_ACCESS_TOKEN: { status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "TOKEN4001", "message": "존재하지 않는 엑세스 토큰 입니다." },
     REFRESH_TOKEN_EXPIRED: { status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "TOKEN4002", "message": "만료된 리프레시 토큰 입니다." },
     ACCESS_TOKEN_NOT_EXPIRED: { status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "TOKEN4003", "message": "엑세스 토큰이 만료되지 않았습니다." },
-    MISSING_TOKEN: { status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "TOKEN4004", "message": "헤더에 토큰 값이 존재하지 않습니다." },
     ACCESS_TOKEN_EXPIRED: { status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "TOKEN4005", "message": "엑세스 토큰이 만료되었습니다." },
     INVALID_REFRESH_TOKEN: { status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "TOKEN4005", "message": "유효하지 않는 리프레시 토큰입니다." },
 
