031e58ad2b09158eee3806f8ab8f12da9a827f89
OSSU <o.ss.u2056@gmail.com>
2024-08-18 12:30:03 +0900

    🐛 Fix(#78): 충돌 이상한 부분 해결

--- src/communities/communities.service.js
+++ src/communities/communities.service.js
@@ -1,34 +1,7 @@
 import { BaseError } from '../../config/error.js';
 import { status } from '../../config/response.status.js';
-
-import {
-    deleteCommunityDao,
-    checkCommunityExistenceDao,
-    checkCommunityOwnerDao,
-    getCommunities,
-    getCommunityCurrentCountDao,
-    getCommunityCapacityDao,
-    joinCommunity,
-    checkUserInCommunity,
-    rejoinCommunity,
-    leaveCommunityDao,
-    getCommunityDetailsDao,
-    getChatroomDetailsDao,
-    updateMeetingDetailsDao,
-    getCommunityUpdatedAtDao,
-    searchCommunitiesByTagKeyword,
-    searchCommunitiesByTitleKeyword,
-    checkIfLeaderDao,
-    getMyCommunities,
-    createCommunity,
-    isUserAlreadyInCommunity,
-    deleteCommunityDao,
-    checkCommunityExistenceDao,
-    checkCommunityOwnerDao,
-    isPossibleCreateCommunity,
-} from './communities.dao.js';
-import { getCommunitiesDto, getCommunityDetailsDto, getChatroomDetailsDto, communitiesInfoDTO, mycommunitiesInfoDTO } from './communities.dto.js';
-import { pageInfo } from '../../config/pageInfo.js';
+import * as dao from './communities.dao.js';
+import { getCommunityDetailsDto, getChatroomDetailsDto, communitiesInfoDTO, mycommunitiesInfoDTO } from './communities.dto.js';
 
 
 // 커뮤니티 생성 서비스
@@ -55,12 +28,12 @@ export const createCommunityService = async (community) => {
     }
 
     // 방장이 모임 생성 가능한지 확인
-    if(!isPossibleCreateCommunity(community.user_id, community.book_id)) {
+    if(!dao.isPossibleCreateCommunity(community.user_id, community.book_id)) {
         throw new BaseError(status.COMMUNITY_LIMIT_EXCEEDED);
     }
 
     // 모임 생성
-    return await createCommunity(community);
+    return await dao.createCommunity(community);
 };
 
 //커뮤니티 가입 서비스
@@ -68,7 +41,7 @@ export const joinCommunityService = async (communityId, userId) => {
     // 유저가 커뮤니티에 이미 존재하는지 확인하고 is_deleted 상태 반환
     const userStatus = await checkUserInCommunity(communityId, userId);
     // 커뮤니티의 현재 인원수 및 최대 인원수 조회
-    const currentCount = await getCommunityCurrentCountDao(communityId);
+    const currentCount = await dao.getCommunityCurrentCountDao(communityId);
 
     // 현재 인원수가 최대 인원수를 초과하면 오류 발생
     const capacity = await getCommunityCapacityDao(communityId);
@@ -77,10 +50,10 @@ export const joinCommunityService = async (communityId, userId) => {
     }
     if (userStatus === null) {
         // 디비에 유저 정보가 없으면 새로 가입 처리
-        await joinCommunity(communityId, userId);
+        await dao.joinCommunity(communityId, userId);
     } else if (userStatus) {
         // 유저가 탈퇴한 경우, 재가입 처리
-        await rejoinCommunity(communityId, userId);
+        await dao.rejoinCommunity(communityId, userId);
     } else {
         // 유저가 이미 가입되어 있는 경우 오류 발생
         throw new BaseError(status.ALREADY_IN_COMMUNITY);
@@ -90,7 +63,7 @@ export const joinCommunityService = async (communityId, userId) => {
 
 // 전체 모임 리스트 조회
 export const getCommunitiesService = async (offset, limit) => {
-    const allCommunities = await getCommunities(offset, limit);
+    const allCommunities = await dao.getCommunities(offset, limit);
 
     // DTO 내부 로직으로 처리
     const allCommunitiesDTOList = allCommunities.map(c => communitiesInfoDTO(c));
@@ -100,7 +73,7 @@ export const getCommunitiesService = async (offset, limit) => {
 
 // 나의 참여 모임 리스트 조회
 export const getMyCommunitiesService = async (myId, offset, limit) => {
-    const myCommunities = await getMyCommunities(myId, offset, limit);
+    const myCommunities = await dao.getMyCommunities(myId, offset, limit);
 
     // DTO 내부 로직으로 처리
     const myCommunitiesDTOList = myCommunities.map(c => mycommunitiesInfoDTO(c));
@@ -117,14 +90,8 @@ export const searchCommunityService = async (keyword, offset, limit) => {
 
     // 태그 또는 제목 검색
     const searchCommunities = isTagSearch 
-        ? await searchCommunitiesByTagKeyword(decodedKeyword.substring(1), offset, limit) // 태그 검색 ('#은 제거')
-        : await searchCommunitiesByTitleKeyword(decodedKeyword, offset, limit); // 제목 검색
-
-    // DTO 내부 로직으로 처리
-    const searchCommunitiesDTOList = searchCommunities.map(c => communitiesInfoDTO(c));
-
-    return searchCommunitiesDTOList; // 결과 리스트 반환
-};
+        ? await dao.searchCommunitiesByTagKeyword(decodedKeyword.substring(1), offset, limit) // 태그 검색 ('#은 제거')
+        : await dao.searchCommunitiesByTitleKeyword(decodedKeyword, offset, limit); // 제목 검색
 
     // DTO 내부 로직으로 처리
     const searchCommunitiesDTOList = searchCommunities.map(c => communitiesInfoDTO(c));
@@ -133,17 +100,17 @@ export const searchCommunityService = async (keyword, offset, limit) => {
 };
 
 export const deleteCommunityService = async (user_id, community_id) => {
-    const exists = await checkCommunityExistenceDao(community_id);
+    const exists = await dao.checkCommunityExistenceDao(community_id);
     if (!exists) {
         throw new BaseError(status.COMMUNITY_NOT_FOUND);
     }
 
-    const owner = await checkCommunityOwnerDao(community_id);
+    const owner = await dao.checkCommunityOwnerDao(community_id);
     if (owner !== user_id) {
         throw new BaseError(status.UNAUTHORIZED);
     }
 
-    await deleteCommunityDao(community_id);
+    await dao.deleteCommunityDao(community_id);
 };
   
 export const leaveCommunityService = async (communityId, userId) => {
@@ -155,19 +122,19 @@ export const leaveCommunityService = async (communityId, userId) => {
     }
 
     // 유저가 방장인지 확인
-    const isLeader = await checkIfLeaderDao(communityId, userId);
+    const isLeader = await dao.checkIfLeaderDao(communityId, userId);
     if (isLeader) {
         // 방장은 탈퇴할 수 없음
         throw new BaseError(status.LEADER_CANNOT_LEAVE);
     }
 
     // 유저 탈퇴 처리 (소프트 딜리트 및 삭제 시간 기록)
-    await leaveCommunityDao(communityId, userId);
+    await dao.leaveCommunityDao(communityId, userId);
 };
 
 // 커뮤니티 상세정보를 가져오는 서비스 함수
 export const getCommunityDetailsService = async (communityId, userId) => {
-    const communityData = await getCommunityDetailsDao(communityId);
+    const communityData = await dao.getCommunityDetailsDao(communityId);
     let isUserParticipating = false;
 
 
@@ -191,7 +158,7 @@ export const getChatroomDetailsService = async (communityId, currentUserId) => {
     }
 
     // 커뮤니티 데이터 가져오기
-    const { communityData, membersData } = await getChatroomDetailsDao(communityId);
+    const { communityData, membersData } = await dao.getChatroomDetailsDao(communityId);
     if (!communityData || communityData.length === 0) {
         throw new BaseError(status.NOT_FOUND);
     }
@@ -202,12 +169,12 @@ export const getChatroomDetailsService = async (communityId, currentUserId) => {
 
 export const updateMeetingDetailsService = async (communityId, meetingDate, latitude, longitude, address, userId) => {
 
-    const exists = await checkCommunityExistenceDao(communityId);
+    const exists = await dao.checkCommunityExistenceDao(communityId);
     if (!exists) {
         throw new BaseError(status.COMMUNITY_NOT_FOUND);
     }
 
-    const communityUpdatedAt = await getCommunityUpdatedAtDao(communityId);
+    const communityUpdatedAt = await dao.getCommunityUpdatedAtDao(communityId);
     const updatedAtDate = new Date(communityUpdatedAt);
     const meetingDateDate = new Date(meetingDate);
     const thirtyMinutesInMs = 30 * 60 * 1000;
@@ -218,5 +185,5 @@ export const updateMeetingDetailsService = async (communityId, meetingDate, lati
     }
 
 
-    await updateMeetingDetailsDao(communityId, meetingDate, latitude, longitude, address, userId);
+    await dao.updateMeetingDetailsDao(communityId, meetingDate, latitude, longitude, address, userId);
 };
