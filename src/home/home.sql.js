6d150214714606d178ccf5a00bbb94f188fc1d57
OSSU <o.ss.u2056@gmail.com>
2024-08-21 08:11:45 +0900

    HOTFIX: 유저가 팔로잉하는 사람들이 올린 피드 리스트 반환 쿼리 수정

--- src/home/home.sql.js
+++ src/home/home.sql.js
@@ -105,17 +105,16 @@ LIMIT ? OFFSET ?;
 
 // 유저의 팔로워들이 올린 피드 리스트 반환
 export const getFollowerFeed = 
-`SELECT 
-u.user_id, u.image_url AS profileImg, 
-u.nickname, s.shorts_id, s.image_url AS shortsImg, 
-s.phrase, s.phrase_x, s.phrase_y, s.title, s.content, s.tag, 
-COALESCE(likes.like_count, 0) AS likeCnt,
-COALESCE(comments.comment_count, 0) AS commentCnt,
-s.created_at, 
-EXISTS(SELECT 1 FROM LIKE_SHORTS WHERE shorts_id = s.shorts_id AND user_id = ?) AS isLike 
-FROM USERS u 
-JOIN FOLLOW f ON u.user_id = f.user_id
-JOIN SHORTS s ON u.user_id = s.user_id 
+`SELECT
+    u.user_id, u.image_url AS profileImg, u.account,
+    s.shorts_id, s.image_url AS shortsImg, s.phrase, s.phrase_x, s.phrase_y, s.title, s.content, s.tag,
+    COALESCE(likes.like_count, 0) AS likeCnt,
+    COALESCE(comments.comment_count, 0) AS commentCnt,
+    s.created_at,
+    EXISTS(SELECT 1 FROM LIKE_SHORTS WHERE shorts_id = s.shorts_id AND user_id = ?) AS isLike
+FROM SHORTS s
+LEFT JOIN USERS u ON s.user_id = u.user_id
+LEFT JOIN FOLLOW f ON u.user_id = f.user_id
 LEFT JOIN (
     SELECT shorts_id, COUNT(*) AS like_count
     FROM LIKE_SHORTS
@@ -126,9 +125,8 @@ LEFT JOIN (
     FROM COMMENT
     GROUP BY shorts_id
 ) comments ON s.shorts_id = comments.shorts_id
-WHERE f.user_id = ? 
-AND s.created_at >= NOW() - INTERVAL 24 HOUR
-GROUP BY u.user_id, u.image_url, u.nickname, s.shorts_id, s.image_url, s.phrase, s.title, s.content, s.tag, s.created_at
+WHERE f.follower = ? AND s.created_at >= NOW() - INTERVAL 24 HOUR
+GROUP BY s.shorts_id, s.created_at
 ORDER BY s.created_at DESC
 LIMIT ? OFFSET ?;
 `;
